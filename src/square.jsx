297734cd8d7613e3178f9545d3d79cba5f24af96
cwklausing <cwklausing@gmail.com>
2017-07-01 11:07:19 -0500

    add clicking to squares

--- src/square.jsx
+++ src/square.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 class Square extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			value: null
+		};
+	}
+
 	render() {
 		return (
-			<div className="square">
-				{this.props.value}
+			<div className="square" onClick={() => this.setState({ value: 'X' })}>
+				{this.state.value}
 			</div>
 		);
 	}
