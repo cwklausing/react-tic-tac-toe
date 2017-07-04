2a44be8165315d89f3cd590e72a9300cab5e9695
cwklausing <cwklausing@gmail.com>
2017-07-04 17:31:37 -0500

    move squares state to board

--- src/board.js
+++ src/board.js
@@ -2,23 +2,40 @@ import React from 'react';
 import Square from './square';
 
 class Board extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			squares: Array(9).fill(null)
+		};
+	}
+
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({ squares: squares });
+	}
+
+	renderSquare(i) {
+		return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+	}
+
 	render(i) {
 		return (
 			<div className="board">
 				<div className="board-row row-1">
-					<Square />
-					<Square />
-					<Square />
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
 				</div>
 				<div className="board-row row-2">
-					<Square />
-					<Square />
-					<Square />
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
 				</div>
 				<div className="board-row row-3">
-					<Square />
-					<Square />
-					<Square />
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
 				</div>
 			</div>
 		);
--- src/square.jsx
+++ src/square.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 
 class Square extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			value: null
-		};
-	}
-
 	render() {
 		return (
-			<div className="square" onClick={() => this.setState({ value: 'X' })}>
-				{this.state.value}
+			<div className="square" onClick={() => this.props.onClick()}>
+				{this.props.value}
 			</div>
 		);
 	}
